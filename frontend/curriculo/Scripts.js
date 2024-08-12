c424d22775c058683ef9fdffcc324a977b67954f
Gustavo Espindola Oselame <143852278+gustavoespindolaoselame@users.noreply.github.com>
2024-08-12 17:13:28 -0300

    commit

--- frontend/curriculo/Scripts.js
+++ frontend/curriculo/Scripts.js
@@ -1,61 +1,90 @@
-const navimage = document.querySelectorAll('.navimage');
-const buttons = document.querySelectorAll('.navbutton');
+class navButtonClass {
+    constructor(navimage, ishovered, isscrolled, isclicked){
+        this.navimage = navimage;
+        this.ishovered = ishovered;
+        this.isscrolled = isscrolled;
+        this.isclicked = isclicked;
+    }
+}
+
+const navButton = new navButtonClass(document.querySelectorAll('.navimage'), [], [], [])
+const overlayOpener = document.querySelectorAll('.overlayOpener');
 const sections = document.querySelectorAll('section');
 
-function FilterOverlay(bbdisplay, button) {
-    this.element = bbdisplay;
-    this.button = button;
-};
-
-const filterconst = new FilterOverlay([], [], []);
-
 document.addEventListener('DOMContentLoaded', () => {
-    for (let index = 0; index < navimage.length; index++) {
-        filterconst.element[index] = document.createElement('div');
-        filterconst.element[index].className = 'cover';
-        filterconst.element[index].style.width = `${navimage[index].offsetWidth}px`;
-        filterconst.element[index].style.height = `${navimage[index].offsetHeight}px`;
-        filterconst.element[index].style.top = `${navimage[index].offsetTop}px`;
-        filterconst.element[index].style.left = `${navimage[index].offsetLeft}px`;
-        filterconst.element[index].classList.add('new-element');
-        navimage[index].parentNode.appendChild(filterconst.element[index]);
-        filterconst.button[index] = buttons[index];
-        filterconst.element[0].style.opacity = '0';
-    };
-    toggleTitleBracket();
+    navButton.isscrolled[0]=true;
+    titleOpenTextBracket();
+    drawOpacityChange();
 });
 
 window.addEventListener('click', () => {
-    for (let index = 0; index < navimage.length; index++) {
-        filterconst.button[index].addEventListener('click', () => {
+    for (let index = 0; index < overlayOpener.length; index++) {
+        overlayOpener[index].addEventListener('click', () => {
+            const test = this.getAttribute('data-index');
+            document.getElementById('overlay-' + test).style.display = 'flex';
+            console.log("que");
+        });
+    }
+    for (let index = 0; index < navButton.navimage.length; index++) {
+        navButton.navimage[index].addEventListener('click', () => {
             window.scrollTo({
                 top: `${sections[index].offsetTop}`,
                 behavior: 'smooth'
             });
+            navButton.isclicked[index]=true;
+        });
+        navButton.isclicked[index]=false;
+    }
+    drawOpacityChange();
+});
+
+window.addEventListener('mouseover', () => {
+    for (let index = 0; index < navButton.navimage.length; index++) {
+        navButton.navimage[index].addEventListener('mouseover', () => {        
+            navButton.ishovered[index]=true;
+            navButton.navimage[index].style.cursor = 'pointer';
+        });   
+        navButton.navimage[index].addEventListener('mouseout', () => {        
+                navButton.ishovered[index]=false;
         });
     }
+    drawOpacityChange();
 });
+
 window.addEventListener('scroll', () => {
-    for (let index = 0; index < navimage.length; index++) {
-        if (window.scrollY >= sections[index].offsetTop - 300 && window.scrollY < sections[index + 1].offsetTop - 300) {
-            filterconst.element[index].style.opacity = '0';
-        } else if (window.scrollY >= sections[navimage.length - 1].offsetTop - 300) {
-            filterconst.element[navimage.length - 1].style.opacity = '0';
-            filterconst.element[navimage.length - 2].style.opacity = '0.8';
-        } else {
-            filterconst.element[index].style.opacity = '0.8';
+    for (let index = 0; index < navButton.navimage.length; index++) {
+        if (window.scrollY > `${sections[index].offsetTop}`-`${sections[index].clientHeight}`/2)
+        {
+            navButton.isscrolled[index]=true;
+            navButton.isscrolled[index-1]=false;
+        }  else{
+            navButton.isscrolled[index]=false;
         }
-    }
+    }   
+    drawOpacityChange();
 });
 
-function toggleTitleBracket() {
-    const titlebracket = document.getElementById('titlebracket');
-    const newH2 = document.createElement('h2');
-    newH2.textContent = '|';
-    
-    titlebracket.appendChild(newH2);
+function drawOpacityChange(){
+    for (let index = 0; index < navButton.navimage.length; index++) {
+        navButton.navimage[index].style.opacity = '0.1';
+        if(navButton.isscrolled[index]){
+            navButton.navimage[index].style.opacity = '1';
+        } 
+        else if(navButton.ishovered[index]){
+            navButton.navimage[index].style.opacity = '0.333';
+        } 
+        else if(navButton.isclicked[index]){
+            navButton.navimage[index].style.opacity = '1';
+        }
+    }   
+}
 
+function titleOpenTextBracket(){
+    let titlebracket = document.getElementById('titlebracket');
+    let newSpan = document.createElement('span');
+    newSpan.textContent = '|';
+    titlebracket.appendChild(newSpan);
     setInterval(() => {
-        newH2.style.visibility = newH2.style.visibility === 'hidden' ? 'visible' : 'hidden';
+        newSpan.style.visibility = newSpan.style.visibility === 'hidden' ? 'visible' : 'hidden';
     }, 500);
 }
