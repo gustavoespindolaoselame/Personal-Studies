18665f861f5b0c01d0b9c0201ddbba859bb1b2a7
Gustavo Espindola Oselame <143852278+gustavoespindolaoselame@users.noreply.github.com>
2024-10-01 14:12:43 -0300

    commit

---
+++ JavaDatabaseTest/app/src/main/java/javadatabasetest/Parameters.java
@@ -0,0 +1,7 @@
+package javadatabasetest;
+import java.util.Scanner;
+public class Parameters {
+    Scanner scanner = new Scanner(System.in);
+    Methods buildDB = new Methods();
+    int choice;
+}
