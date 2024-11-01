2b39b0cb6496afa427fd6a7a0842df571927ca0d
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2024-10-31 22:36:45 -0300

    fixed slight changes by missclick

--- DesafioStreamCod3r/src/main/java/org/gustavogustavo/desafiostream/DesafioStreamCod3r.java
+++ DesafioStreamCod3r/src/main/java/org/gustavogustavo/desafiostream/DesafioStreamCod3r.java
@@ -35,7 +35,7 @@ public class DesafioStreamCod3r {
             Integer result = 0;
             int length = binary.length();
             for (int i = 0; i < length; i++) {
-                result += (binary.charA t(length - i - 1) - '0') * (int)Math.pow(2, i);
+                result += (binary.charAt(length - i - 1) - '0') * (int)Math.pow(2, i);
             }
             return result;
         };
