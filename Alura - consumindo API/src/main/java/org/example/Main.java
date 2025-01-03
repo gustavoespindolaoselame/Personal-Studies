abb8f35f9e226d492dcb6b1b10ace9ff01e66491
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2025-01-03 10:41:35 -0300

    Added Alura Studies

---
+++ Alura - consumindo API/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=933fcca4";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException | InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
