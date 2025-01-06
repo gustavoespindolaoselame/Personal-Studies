bb34bcbf968e830ef8db0b279c7fe659da623659
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2025-01-06 12:54:36 -0300

    Added Alura Studies+

--- Alura - consumindo API/src/main/java/org/example/Main.java
+++ Alura - consumindo API/src/main/java/org/example/Main.java
@@ -1,29 +1,58 @@
 package org.example;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para busca: ");
-        var busca = leitura.nextLine();
-
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=933fcca4";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
+        FileWriter escrita = null;
         try {
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-        } catch (IOException | InterruptedException e){
+            escrita = new FileWriter("filmes.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Digite um filme para busca: ");
+        String busca = leitura.nextLine();
+        while(!busca.equalsIgnoreCase("sair")) {
+            String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=933fcca4";
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            try {
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                String json = response.body();
+                Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                escrita.write(meuTitulo.toString() + "\n");
+                System.out.println(meuTitulo.toString());
+
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Digite um filme para busca: ");
+            busca = leitura.nextLine();
+        }
+        leitura.close();
+        try {
+            escrita.close();
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
---
+++ Alura - consumindo API/src/main/java/org/example/Titulo.java
@@ -0,0 +1,102 @@
+package org.example;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Titulo {
+
+    @SerializedName("Title")
+    private String nome;
+
+    @SerializedName("Year")
+    private int anoDeLancamento;
+
+    private boolean incluidoNoPlano;
+
+    private double somaDasAvaliacoes;
+
+    private int totalDasAvaliacoes;
+
+    private int duracaoEmMinutos;
+
+    public Titulo(String nome, int anoDeLancamento, boolean incluidoNoPlano, double somaDasAvaliacoes, int totalDasAvaliacoes, int duracaoEmMinutos) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+        this.incluidoNoPlano = incluidoNoPlano;
+        this.somaDasAvaliacoes = somaDasAvaliacoes;
+        this.totalDasAvaliacoes = totalDasAvaliacoes;
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb){
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year().substring(0, 4));
+        try {
+            try {
+                this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, Integer.valueOf(meuTituloOmdb.runtime().indexOf(' '))));
+            } catch (Exception e) {
+            }
+
+            try {
+                this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
+            } catch (Exception e) {
+            }
+
+        } catch (NumberFormatException e) {
+            this.duracaoEmMinutos = 0;
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public double getSomaDasAvaliacoes() {
+        return somaDasAvaliacoes;
+    }
+
+    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+        this.somaDasAvaliacoes = somaDasAvaliacoes;
+    }
+
+    public int getTotalDasAvaliacoes() {
+        return totalDasAvaliacoes;
+    }
+
+    public void setTotalDasAvaliacoes(int totalDasAvaliacoes) {
+        this.totalDasAvaliacoes = totalDasAvaliacoes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    @Override
+    public String toString() {
+        return  "Nome: '" + nome + "\nanoDeLancamento: " + anoDeLancamento + "\nDuração: " + duracaoEmMinutos;
+    }
+}
