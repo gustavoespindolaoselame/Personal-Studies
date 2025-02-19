bf9dd614ba133e437ec945d5ebc8ab532f607f3e
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2025-02-19 11:09:48 -0300

    react studies

--- Cod3r - REACT & REDUX/DesafioCalculadora/my-app/src/App.jsx
+++ Cod3r - REACT & REDUX/DesafioCalculadora/my-app/src/App.jsx
@@ -13,13 +13,19 @@ export default _ => {
     }
 
     const checkedUpdateScreenValue = appendedString => {
-        operations.forEach(operation => {
-            if (screenValueString.endsWith(operation) && appendedString === operation) {
-                appendedString = '';
-            }
+        operations.forEach(operation1 => {
+            operations.forEach(operation2 => {
+                if (screenValueString.endsWith(operation1) && appendedString === operation2) {
+                    if(!(appendedString === '-' && !screenValueString.endsWith('-'))){
+                        appendedString = '';
+                    }
+                }
+            })
+            
         });
+
         let finalString = screenValueString + appendedString;
-        if(finalString.startsWith('0')){
+        if(finalString.startsWith('0') && !operations.includes(finalString.charAt(1)) && finalString.charAt(1)!='.'){
             finalString = finalString.slice(1);
         }
         updateScreenValue(finalString);
@@ -27,23 +33,46 @@ export default _ => {
 
     const runEquation = _ => {Logic(screenValueString, updateScreenValue);}
 
+    const removeLast = _ => {
+        
+        if(screenValueString.length==1){
+        setScreenValueString('0');
+        } else {
+            setScreenValueString(screenValueString.slice(0, -1));
+        }
+    }
+
+    const removeAll = _ => {
+        setScreenValueString('0');
+    }
+
+    const addDot = _ => {
+        setScreenValueString(screenValueString+'.');
+    }
+
     return (
         <div id='container'>
         <NPScreen>{screenValueString}</NPScreen>
-        <NPButton num={'/'} action={checkedUpdateScreenValue}/>
-        <NPButton num={'-'} action={checkedUpdateScreenValue}/>
-        <NPButton num={'+'} action={checkedUpdateScreenValue}/>
+        
+        
+        
         <NPButton num={'9'} action={checkedUpdateScreenValue}/>
         <NPButton num={'8'} action={checkedUpdateScreenValue}/>
         <NPButton num={'7'} action={checkedUpdateScreenValue}/>
+        <NPButton num={'+'} action={checkedUpdateScreenValue}/>
         <NPButton num={'6'} action={checkedUpdateScreenValue}/>
         <NPButton num={'5'} action={checkedUpdateScreenValue}/>
         <NPButton num={'4'} action={checkedUpdateScreenValue}/>
+        <NPButton num={'-'} action={checkedUpdateScreenValue}/>
         <NPButton num={'3'} action={checkedUpdateScreenValue}/>
         <NPButton num={'2'} action={checkedUpdateScreenValue}/>
         <NPButton num={'1'} action={checkedUpdateScreenValue}/>
         <NPButton num={'*'} action={checkedUpdateScreenValue}/>
+        <NPButton num={'del'} action={removeAll}/>
         <NPButton num={'0'} action={checkedUpdateScreenValue}/>
+        <NPButton num={'.'} action={addDot}/>
+        <NPButton num={'/'} action={checkedUpdateScreenValue}/>   
+        <NPButton num={'bac'} action={removeLast}/>   
         <NPButton num={'='} action={runEquation}/>
         </div>
     )
--- Cod3r - REACT & REDUX/DesafioCalculadora/my-app/src/logic.js
+++ Cod3r - REACT & REDUX/DesafioCalculadora/my-app/src/logic.js
@@ -1,12 +1,45 @@
 export default (stringParam, callBackUpdate) => {
     const runEquation = (_) => {
-        const divide = (n1, n2) => parseFloat(n1)/parseFloat(n2)
-        const multiply = (n1, n2) => parseFloat(n1)*parseFloat(n2)
-        const sum = (n1, n2) => parseFloat(n1)+parseFloat(n2)
-        const subtract = (n1, n2) => parseFloat(n1)-parseFloat(n2)
-        let parsedValues = stringParam.match(/\d+(\.\d+)?|[+\-*/]/g);
 
-        while(parsedValues.length>1){
+        const operations = ['/','*','-','+']
+        const divide = (n1, n2) => parseFloat(n1)/parseFloat(n2);
+        const multiply = (n1, n2) => parseFloat(n1)*parseFloat(n2);
+        const sum = (n1, n2) => parseFloat(n1)+parseFloat(n2);
+        const subtract = (n1, n2) => parseFloat(n1)-parseFloat(n2);
+
+        let parseValues = () => {
+
+            operations.forEach(operation =>{
+                if(stringParam.endsWith(operation)){
+                    if(operation=='/'||operation=='*'){
+                        stringParam = stringParam + '1'
+                    } else {
+                        stringParam = stringParam + '0'
+                    }
+                }
+            })
+
+            let returnParsedValues = stringParam.match(/\d+(\.\d+)?|[+\-*/]/g);
+            for(let i = 0; i<returnParsedValues.length; i++){
+                operations.forEach(operation => {
+                    if(returnParsedValues[i]=='-'&&returnParsedValues[i-1]==operation){
+                        returnParsedValues[i+1]=`${-returnParsedValues[i+1]}`;
+                        returnParsedValues.splice(i,1);
+                    }
+                    if(returnParsedValues[0]=='-'||returnParsedValues[0]=='+'){
+                        returnParsedValues.unshift('0');
+                    }
+                })
+            }
+            console.log(returnParsedValues)
+            return returnParsedValues;
+        };
+
+        let parsedValues = parseValues();
+
+        let tries = 0;
+        while(parsedValues.length>1 && tries<100){
+            tries++;
             let finalVal = 0;
             switch(parsedValues[1]){
                 case '/':
