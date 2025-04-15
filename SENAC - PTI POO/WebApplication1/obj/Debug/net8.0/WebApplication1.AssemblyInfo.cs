56b410a5b86b499a72481b3616a83e0d989c98db
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2025-04-15 15:38:48 -0300

    project

--- SENAC - PTI POO/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ SENAC - PTI POO/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09e2975ae596c2eb3cb3db139bffe63b12b46c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0be698593530d99980e54e2a7606e2d3af9ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
