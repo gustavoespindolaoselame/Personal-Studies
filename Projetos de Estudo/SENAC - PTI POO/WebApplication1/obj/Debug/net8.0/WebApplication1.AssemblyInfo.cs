ecc29f308ae194ee2fdd79de354cf0fdb53f9be8
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2025-05-12 12:02:01 -0300

    big studies

--- Projetos de Estudo/SENAC - PTI POO/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Projetos de Estudo/SENAC - PTI POO/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443c0fd3abe2ebdeb4056cbc7544adb6ab5ab97f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c23d3abcaa4408b7fafd7f8fce39a88dd71b33")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
